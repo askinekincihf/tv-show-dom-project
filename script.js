4be419ab509a60760432ab74284e4d8f07fbf673
askinekinci <askinekincihf@gmail.com>
2021-03-17 23:07:02 +0000

    added extra features

--- script.js
+++ script.js
@@ -32,9 +32,9 @@ function makePageForEpisodes(episodeList) {
 
   // Show default Display
   episodeList.forEach(episode => {
-    const episodeSeason = `${episode.season > 9 ? episode.season : "0" + episode.season}`
-    const episodeNumber = `${episode.number > 9 ? episode.number : "0" + episode.number}`
-    const episodeTitle = `${episode.name} - S${episodeSeason}E${episodeNumber}`
+    const episodeSeason = `${episode.season > 9 ? episode.season : "0" + episode.season}`;
+    const episodeNumber = `${episode.number > 9 ? episode.number : "0" + episode.number}`;
+    const episodeTitle = `${episode.name} - S${episodeSeason}E${episodeNumber}`;
     wrapper.innerHTML += `
         <div class="col card-wrapper">
           <div class="card bg-light p-3 h-100 pt-3">
@@ -42,32 +42,43 @@ function makePageForEpisodes(episodeList) {
               <h5 class="card-title title d-flex justify-content-center text-center">${episodeTitle}</h5>
             </div>
             <img src="${episode.image.medium}" class="img mb-2 mt-2 px-3" alt="${episode.name}" />
-            <p class="summary">${episode.summary}</p>
-            <a href="${episode.url}">${episode.url}</a>
+            ${episode.summary}
+            <a href="${episode.url}">See Details</a>
           </div>
         </div>`
 
-    const dropDownEpisodeTitle = `S${episodeSeason}E${episodeNumber} - ${episode.name}`
+    const dropDownEpisodeTitle = `S${episodeSeason}E${episodeNumber} - ${episode.name}`;
     const optionValue = episodeTitle;
     dropdownMenu.innerHTML += `
-          <option class="select-option" value="${optionValue}">${dropDownEpisodeTitle}</option>;
+          <option class="select-option" value="${optionValue}">${dropDownEpisodeTitle}</option>
         `
   })
 
   // Events
   searchField.addEventListener("input", searchEpisodes);
   dropdownMenu.addEventListener("change", searchDropdown);
+  searchField.addEventListener("click", resetSearch);
 
   // Functions
   function searchEpisodes() {
+    const searchFieldValue = searchField.value;
+    let value = searchFieldValue.toLowerCase();
+    search(value);
+  }
+
+  function searchDropdown(e) {
+    const dropdownMenuValue = e.currentTarget.value;
+    let value = dropdownMenuValue.toLowerCase();
+    search(value);
+  }
+
+  function search(value) {
     countEpisodes.innerText = "";
     let count = 0;
-    const searchFieldValue = searchField.value
-    let searchValue = searchFieldValue.toLowerCase();
     const allCards = document.querySelectorAll(".card-wrapper");
     allCards.forEach(card => {
       const cardValue = card.textContent.toLowerCase();
-      if (cardValue.includes(searchValue)) {
+      if (cardValue.includes(value)) {
         card.style.display = "";
         count++;
         countEpisodes.innerText = `Display ${count} of ${allCards.length} episodes`;
@@ -78,24 +89,9 @@ function makePageForEpisodes(episodeList) {
     })
   }
 
-  function searchDropdown(e) {
-    countEpisodes.innerText = "";
-    let count = 0;
-    const dropdownMenuValue = e.currentTarget.value;
-    let value = dropdownMenuValue.toLowerCase();
-    const allCards = document.querySelectorAll(".card-wrapper");
-    allCards.forEach(card => {
-      const cardValue = card.textContent.toLowerCase();
-      console.log(cardValue)
-      if (cardValue.includes(value)) {
-        card.style.display = "";
-        count++;
-        countEpisodes.innerText = `Display ${count} of ${allCards.length} episodes`;
-      } else {
-        card.style.display = "none";
-        countEpisodes.innerText = `Display ${count} of ${allCards.length} episodes`;
-      }
-    })
+  function resetSearch() {
+    dropdownMenu.selectedIndex = 0;
+    searchEpisodes();
   }
 
 }
